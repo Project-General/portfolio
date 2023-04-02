5551290bbf2cf678e3881604a2e899d10a2997f4
Kas Awan <qasim.a.k786@gmail.com>
2023-04-02 19:19:27 +0100

    background colour set

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const myTags = [
+  'HTML',
+  'CSS',
+  'JS',
+  'C#',
+  'Java',
+  '.NET',
+  'TypeScript',
+  'React',
+  'Vue',
+  'GraphQL',
+  'REST'
+];
+
+var tagCloud = TagCloud('.content', myTags);
